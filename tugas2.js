644177c5dc4dc01cfd27be1e993f427676b06f25
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-10-24 14:32:42 +0700

    Add files via upload

--- tugas2.js
+++ tugas2.js
@@ -3,11 +3,17 @@
 // Oct 24, 2023
 
 const bahasaIndonesia = 95;
-const mtk = 90;
-const bahasaInggris = 90;
-const ipa = 99;
+const mtk = 80;
+const bahasaInggris = 85;
+const ipa = 80;
 
-let rataRata = (bahasaIndonesia + mtk + bahasaInggris + ipa) / 4;
+let mataPelajaran = [bahasaIndonesia, mtk, bahasaInggris, ipa];
+let nilai = 0;
+for (let i = 0; i < mataPelajaran.length; i++) {
+  nilai += mataPelajaran[i];
+}
+
+let rataRata = nilai / mataPelajaran.length;
 
 if (rataRata >= 90) {
   console.log(`Rata-rata  = ${rataRata}
