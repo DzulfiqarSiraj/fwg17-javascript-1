494be539742e4e3eef5139900e450b8efb2a5fe1
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-10-29 00:21:19 +0700

    Add files via upload

--- tugas3.js
+++ tugas3.js
@@ -3,9 +3,12 @@
 // Oct 24, 2023
 
 let printSegitiga = 5;
+if(typeof printSegitiga != 'number'){
+  console.log('Input harus berupa angka')
+}
 
 for (let i = printSegitiga; i > 0; i--) {
-  let result = "";
+  let result = ""; //concatenation
   for (let j = 1; j <= i; j++) {
     result += j;
   }
